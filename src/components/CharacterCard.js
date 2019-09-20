47c727fd1f91a559f96a57c223a775198fbf1bcb
John Wells <molekewl2017@gmail.com>
2019-09-20 09:38:15 -0700

    able to display data, name & image populates

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ name, image}) {
+  return (
+    <div>
+      <img src={image}/>
+      <p>Name: {name}</p>
+
+    </div>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
+
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [data, setData] = useState([]);
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        console.log(res);
+        const characters = res.data.results;
+        setData(characters);
+      })
+      .catch(err => {
+        console.log('No data return', err);
+      })
   }, []);
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      {data.map(character => (
+        <CharacterCard
+          key={character.id}
+          name={character.name}
+          image={character.image}
+        />
+      ))}
     </section>
   );
 }
